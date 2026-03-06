cdcca412f548e6de539b1bfeadbeaa8906165410
Sepecter <1170724164@qq.com>
2026-03-06 16:54:49 +0800

    compiler

--- src/fitness/cpp_code_gen_v2/code_eval.py
+++ src/fitness/cpp_code_gen_v2/code_eval.py
@@ -65,8 +65,8 @@ def compile_code(code):
     file_path = path.join(path_1, now + '.cpp')
     cpp_file = file_path
 
-    compiler1 = 'g++-16'
-    compiler2 = 'clang++-trunk'
+    compiler1 = 'g++16-cov'
+    compiler2 = 'clang++-23-cov'
 
     output_file = path.join(output_dir, now)
 
